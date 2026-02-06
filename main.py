886882fcd134e191b54717df9766e9b299789483
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-02-06 15:26:24 +0530

    Completed Succesfully

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 from routes.user_routes import router as user_router
-from db import get_db
+from db import get_db,DATABASE_URL
+from sqlalchemy import create_engine
+from models import Base
+import os
 
 app = FastAPI()
 app.include_router(user_router)
 #to create database
-
+if not os.path.exists("./test.db"):
+    engine=create_engine(DATABASE_URL)
+    Base.metadata.create_all(engine)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 if __name__ == "__main__":
     import uvicorn
